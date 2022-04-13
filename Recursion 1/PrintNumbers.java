49ed0adfae12325fc281fca05c1d2e28fe38008e
Harsh Rajput <77964116+harshrajput688@users.noreply.github.com>
2022-04-13 09:08:03 -0700

    Create PrintNumbers.java

---
+++ Recursion 1/PrintNumbers.java
@@ -0,0 +1,13 @@
+public class PrintNumbers {
+
+	public static void print(int n){
+		if(n == 1){
+			System.out.print(n + " ");
+            return;
+		}
+        print(n - 1);
+		System.out.print(n+" ");
+		// print(n - 1);
+	}
+
+}
